83718b038a58479d1b03576abe9a7f154f0a5055
Daniel Davis <daniel@hsdesignhouse.com>
2017-11-09 16:19:07 -0600

    Remove turbo gem

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,13 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+
+
+Phrasing.setup do |config|
+  config.route = 'p/ph/phrasing'
+end
+
+# List all the model attributes you wish to edit with Phrasing, example:
+# Phrasing.whitelist = ["Post.title", "Post.description"]
+Phrasing.whitelist = []
+Phrasing.allow_update_on_all_models_and_attributes = true;
